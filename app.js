32e46b5e9964772eadae7f7c0016c678bd88951a
Billy Tat <billyt@activestate.com>
2014-07-15 09:54:52 -0700

    Static is now serve-static with Connect 3

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //setup Dependencies
 var connect = require('connect')
     , port = (process.env.PORT || 8081);
+var serveStatic = require('serve-static');
 
 connect()
-    .use(connect.static(__dirname + '/public'))
+    .use(serveStatic(__dirname + '/public'))
     .listen(port);
